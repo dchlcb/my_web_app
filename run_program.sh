0d88709ebf76352447d81bf0651f514ec9507a21
alientek <example@email.com>
2025-05-15 09:32:56 +0800

    general_demo

--- run_program.sh
+++ run_program.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置要执行的应用程序路径
-PROGRAM="./my_web_app"  # 将这里的 your_program 替换为实际的程序路径
+PROGRAM="./my_app"  # 将这里的 your_program 替换为实际的程序路径
 
 # 如果没有传入参数，直接执行应用程序
 if [ "$#" -eq 0 ]; then
